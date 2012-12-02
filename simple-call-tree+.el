73d6925e1e7dd0b9ad5ed38aa9f24238ef88757a
Joe Bloggs <vapniks@yahoo.com>
2012-12-02 20:07:01 +0000

    Slight change to elisp code at end

--- simple-call-tree+.el
+++ simple-call-tree+.el
@@ -1037,5 +1037,6 @@ If ARG is non-nil perform query-replace-regexp instead."
 
 ;;; simple-call-tree+.el ends here
 
-;; (progn (magit-push) (yaoddmuse-post "EmacsWiki" "simple-call-tree+.el" (buffer-name) (buffer-string) "update"))
+;; (magit-push)
+;; (yaoddmuse-post "EmacsWiki" "simple-call-tree+.el" (buffer-name) (buffer-string) "update")
 
