8ee10f5435fc7c426c3ecf48f211a0f728ee2ee5
Joe Bloggs <vapniks@yahoo.com>
2024-07-06 13:42:29 +0100

    append buffer name to simple-call-tree-buffer-name

--- simple-call-tree.el
+++ simple-call-tree.el
@@ -1184,7 +1184,7 @@ be shown in the tree.")
 (defvar simple-call-tree-apply-command-history nil
   "History list for `simple-call-tree-apply-command'.")
 
-;; simple-call-tree-info: CHECK
+;; simple-call-tree-info: CHECK 
 (defun simple-call-tree-rename-buffer (name)
   "Rename the *Simple Call Tree* buffer to NAME.
 The value of `simple-call-tree-buffer-name' will be changed for all buffers in `simple-call-tree-buffers'."
@@ -1599,6 +1599,11 @@ FILES & MAXDEPTH will be prompted for and only functions in the current buffer w
 	     (not (cl-set-exclusive-or
 		   buffers simple-call-tree-buffers)))
 	(switch-to-buffer simple-call-tree-buffer-name)
+      (let ((bufname (concat "*Simple Call Tree: "
+			     (buffer-name (car buffers)) "*")))
+	(dolist (buf buffers)
+	  (with-current-buffer buf
+	    (setq-local simple-call-tree-buffer-name bufname))))
       (simple-call-tree-build-tree buffers)
       (setq simple-call-tree-jump-ring (make-ring simple-call-tree-jump-ring-max)
 	    simple-call-tree-jump-ring-index 0)
@@ -1671,6 +1676,9 @@ otherwise it will be narrowed around FUNC."
 		      simple-call-tree-alist))
 	(if (get-buffer simple-call-tree-buffer-name)
 	    (switch-to-buffer simple-call-tree-buffer-name)
+	  (setq-local simple-call-tree-buffer-name
+		      (concat "*Simple Call Tree: "
+			      (buffer-name (car simple-call-tree-buffers)) "*"))
 	  (simple-call-tree-list-callers-and-functions))
       (when (get-buffer simple-call-tree-buffer-name) ;in case buffer contains different call-tree
 	(kill-buffer simple-call-tree-buffer-name))
